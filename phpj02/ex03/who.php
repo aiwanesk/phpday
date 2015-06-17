09cfd61f7af3143bc8163bec6da77a43a91f08da
Alexandre IWANESKO <aiwanesk@e1r10p7.42.fr>
2015-06-17 19:20:30 +0200

    boom

--- phpj02/ex03/who.php
+++ phpj02/ex03/who.php
@@ -1,18 +1,18 @@
 #!/usr/bin/php
 <?
 date_default_timezone_set('CET');
-$fd = fopen("/var/run/utmpx", "r");
-while ($utmpx = fread($fd, 628))
+$handle = fopen("/var/run/utmpx", "r");
+while ($utmpx = fread($handle, 628))
 {
 	$utmpx_open = unpack("a256user/a4id/a32line/ipid/itype/I2time/a256host/i16pad", $utmpx);
-	if ($utmpx_open["type"] == 7)//si la 7eme case est libre
+	if ($utmpx_open["type"] == 7)
 	{
-		$user[$utmpx_open["line"]] = array("user" => $utmpx_open["user"],"time" => $utmpx_open["time1"]);
+		$user[$utmpx_open["line"]] = array("user" => $utmpx_open["user"], "time" => $utmpx_open["time1"]);
 	}
 }
 ksort($user);
 foreach($user as $line => $data)
 {
-		printf("%s %s %s \n", $data["user"], $line, date(" M j H:i",$data["time"]));
+		printf("% -7s  % -7s  %s \n", $data["user"], $line, date("M  j H:i", $data["time"]));
 }
 ?>
