2e06c5b167a2e38d88d6aa18702a7e4bbad22e82
Alexandre IWANESKO <aiwanesk@e1r11p7.42.fr>
2015-06-19 17:31:58 +0200

    final

--- J03/ex03/cookie_crisp.php
+++ J03/ex03/cookie_crisp.php
@@ -1,13 +1,11 @@
 <?php
 
-if ($_GET['todo'] == "set")
-	setcookie($_GET['todo'], $_GET['value'], time() + 60 * 60 * 24 * 30);
-else if ($_GET['todo'] == "get")
+if ($_GET['action'] == "set")
+	setcookie($_GET['name'], $_GET['value'], time() + 60 * 60 * 24 * 30);
+else if ($_GET['action'] == "get")
 {
-	$tmp = $_COOKIE[$_GET['todo']];
-	if ($tmp)
-		echo $tmp."\n";
+	echo  $_COOKIE[$_GET['name']];
 }
-else if ($_GET['todo'] == "del")
-	setcookie($_GET['todo'], "", time() - 1 );
+else if ($_GET['action'] == "del")
+	setcookie($_GET['name'], "", time() - 1 );
 ?>
